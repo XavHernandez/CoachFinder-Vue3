d433626b84614fb5bd2269d82d49654333f2117d
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-09-24 13:48:06 +0200

    navigation layout and styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-    <router-view>
-    </router-view>
+  <the-header></the-header>
+  <router-view> </router-view>
 </template>
 
 <script>
+import TheHeader from './components/layout/TheHeader.vue';
 export default {
-  name: "App",
-};
+  components: {
+    TheHeader
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
