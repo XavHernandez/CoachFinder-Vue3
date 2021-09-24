2dd4239603f354c4616cc9dcc02f3bdb9eb23387
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-09-24 13:22:35 +0200

    update to vue-router 4

--- src/router.js
+++ src/router.js
@@ -4,24 +4,25 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReiceived from './pages/requests/RequestsReiceived.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: CoachesList },
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: CoachesList },
 
-    { path: '/coaches/:id', component: CoachDetail, children: 
-      [{ path: 'contact', component: ContactCoach }]
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [{ path: "contact", component: ContactCoach }],
     },
 
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestsReiceived },
-    { path: '/:notFound(.*)', component: NotFound }
-  ]
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
 });
 
 export default router;
