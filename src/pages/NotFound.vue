00cb9ae6b7a85ccf323c4b423f07a196703c2801
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-09-27 22:45:02 +0200

    Page 404 added

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <base-card>
+      <h2>404 - Page not found</h2>
+      <p>You can find a coach <router-link to="/coaches">right here!</router-link></p>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
